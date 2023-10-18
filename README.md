013a2c0d87b348aef164a77e349107896afe34c0
lanxin1216 <131834711+lanxin1216@users.noreply.github.com>
2023-10-18 19:16:30 +0800

    创建刷题仓库

---
+++ README.md
@@ -0,0 +1,16 @@
+# leetcode_practice_java
+力扣的刷题代码（java）
+
+[题库 - 力扣 (LeetCode) 全球极客挚爱的技术成长平台](https://leetcode.cn/problemset/all/)
+
+- difficulty_1为困难类型题目
+- easily_1为简单类型题目
+- secondary_1为中等类型题目
+
+## 刷题目录列表：
+
+### easily_1:
+
+- code_01：1. 两数之和
+- code_02：9. 回文数
+- code_03：13. 罗马数字转整数
---
+++ src/easily_1/code_02.java
@@ -0,0 +1,35 @@
+package easily_1;
+/*
+9.回文数
+给你一个整数 x ，如果 x 是一个回文整数，返回 true ；否则，返回 false 。
+回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+例如，121 是回文，而 123 不是。
+ */
+public class code_02 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else {
+            int n = 0;
+            int x01 = x;//判断数字位数。
+            while ( x01 != 0){
+                x01 = x01/10;
+                n++;
+            }
+
+            int num = 0;
+            int num01 = 0;
+            int m = n - 1;
+            x01 = x;
+            for(int i = 0; i < n;i++){
+                num01 = x01 % 10;
+                x01 = x01 / 10;
+                num = num + num01 * (int)Math.pow(10, m);
+                m--;
+            }
+            return num == x;
+        }
+    }
+}
+
+
